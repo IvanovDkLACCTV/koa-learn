b62133a45f8bdb1d82f06ad74687b88d263c3776
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-06-06 17:54:04 +0300

    added router

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const Koa = require("koa")
 const log = console.log //чтобы не писать console.log каждый раз
 const json = require("koa-json") //подключаем модуль для работы с JSON
+const KoaRouter = require("koa-router") //подключаем роутер
 
 const app = new Koa() //подключение к фреймворку
+const router = new KoaRouter() //создаем новый роутер
+
+//JSON prettier Middleware
 app.use(json()) //включаем обработку JSON
 
-//middleware, который отвечает за обработку запросов
-app.use(async (ctx) => (ctx.body = { msg: "Hello World" })) //в таком виде от мы отдаем объект клиенту в формате JSON
+router.get("/test", async (ctx) => {
+  ctx.body = { message: "Hello, World!" } //обрабатываем GET запрос на /test
+  log("GET request to /test") //логируем запрос
+})
+
+//Router Middleware
+app.use(router.routes()) //подключаем роутер к приложению
+app.use(router.allowedMethods()) //подключаем методы роутера
 
 app.listen(3010, () => {
   log("Server running on http://localhost:3010")
