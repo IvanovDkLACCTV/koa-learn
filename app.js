ed00fa4441c5b7b54f8d2b0c0228d5bba4720a12
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-06-06 17:47:51 +0300

    added json lib

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Koa = require("koa")
-
 const log = console.log //чтобы не писать console.log каждый раз
+const json = require("koa-json") //подключаем модуль для работы с JSON
 
 const app = new Koa() //подключение к фреймворку
+app.use(json()) //включаем обработку JSON
 
-app.use(async (ctx) => (ctx.body = "Hello World")) //middleware, который отвечает за обработку запросов
+//middleware, который отвечает за обработку запросов
+app.use(async (ctx) => (ctx.body = { msg: "Hello World" })) //в таком виде от мы отдаем объект клиенту в формате JSON
 
 app.listen(3010, () => {
   log("Server running on http://localhost:3010")
