30d98b10737cf91d808ff2dc49ceb3f0e799cf0b
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-06-10 17:23:22 +0300

    made base layout with ejs

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const Koa = require("koa")
 const log = console.log //чтобы не писать console.log каждый раз
 const json = require("koa-json") //подключаем модуль для работы с JSON
 const KoaRouter = require("koa-router") //подключаем роутер
+const path = require("path") //подключаем модуль для работы с путями
+const render = require("koa-ejs") //подключаем модуль для работы с шаблонами
 
 const app = new Koa() //подключение к фреймворку
 const router = new KoaRouter() //создаем новый роутер
 
 //JSON prettier Middleware
 app.use(json()) //включаем обработку JSON
+
+render(app, {
+  root: path.join(__dirname, "views"), //указываем путь к папке с шаблонами
+  layout: "layout", //указываем имя файла шаблона
+  viewExt: "html", //указываем расширение файлов шаблонов
+  cache: false, //отключаем кэширование шаблонов
+  debug: false, //включаем режим отладки
+})
+
+//Index page
+router.get("/", async (ctx) => {
+  await ctx.render("index", { title: "Welcome to Koa" }) //рендерим шаблон index
+  log("GET request to /") //логируем запрос
+})
 
 router.get("/test", async (ctx) => {
   ctx.body = { message: "Hello, World!" } //обрабатываем GET запрос на /test
